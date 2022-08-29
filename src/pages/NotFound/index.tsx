7d0c174df5d127c1bec8fbfef7b2eda19c444518
Angel Marenco <jsmarenco.dev@gmail.com>
2022-08-28 23:32:20 -0600

    Somes texts were remove

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,16 +1,10 @@
 import React, { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
-
 import { mainContainerStyle, centeredContainerStyle } from "../../styles/containers"
-
-import { Box, Typography, Button } from "@mui/material"
-
-import HomeIcon from "@mui/icons-material/Home"
+import { Box, Typography } from "@mui/material"
 import { changeTitle } from "../../utils"
+import Navbar from "../../components/Navbar"
 
 export default function NotFound() {
-  const navigate = useNavigate()
-
   useEffect(() => {
     changeTitle("Page not found")
   }, [])
@@ -30,15 +24,9 @@ export default function NotFound() {
           >
             <strong>404</strong>
           </Typography>
+
           <Typography
-            variant="h2"
-            color="text.primary"
-            align="center"
-          >
-            Page not found
-          </Typography>
-          <Typography
-            variant="h6"
+            variant="h5"
             color="text.primary"
             align="center"
           >
@@ -46,22 +34,16 @@ export default function NotFound() {
           </Typography>
 
           <Typography
-            variant="h5"
+            variant="h6"
             color="text.primary"
             align="center"
-            mb={2}
           >
-            <strong>What can I do?</strong>
+            What can you do?
           </Typography>
 
-          <Button
-            variant="contained"
-            color="primary"
-            startIcon={<HomeIcon />}
-            onClick={() => navigate("/menu/")}
-          >
-            Go to home page
-          </Button>
+          <Navbar
+            showCancelButton={false}
+          />
         </Box>
       </Box>
     </>
